baa2c713ef9411c531e29a82e470fd8873516730
qianbingbing <qianbingbing@manqian.cn>
2017-06-05 19:58:25 +0800

    解决了json.loads()方法无法导入单引号格式的字典数据，在数据存储到数据库之前就强行将单引号前面加上双引号，这样就符合了json.loads()的规范。

--- apitest/testcase/views.py
+++ apitest/testcase/views.py
@@ -8,6 +8,7 @@ import logging
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 import models
 from django.core.exceptions import ValidationError
+
 # Create your views here.
 logger = logging.getLogger('mylogger')
 #登录页面
@@ -23,65 +24,84 @@ def login_action(request):
             return response
         else:
             return render(request, 'login.html', {'error': 'username or password error!'})
+
+
 #测试报告页面
 def report(request):
     dates=[{"time":"2017-5-11"},{"time":"2017-5-10"},{"time":"2017-5-9"}]
     return render(request,'reportlist.html',{"dates":dates})
+
+
 #跳转添加页面
 def add(request):
     return render(request,'add.html')
+
+
 #保存功能
 def save_testcase(request):
     if request.method == 'POST':
         params = {}
         jsons_data ={}
         testcaseID = int(time.time())
-        title = request.POST.get('title','')
-        desc = request.POST.get('desc','')
-        method = request.POST.get('method','')
-        url = request.POST.get('url','')
-        params_key1 = request.POST.get('key1','')
-        params_value1 = request.POST.get('value1','')
-        ex_result = request.POST.get('ex_result','')
+        title = request.POST.get('title',"")
+        desc = request.POST.get('desc',"")
+        method = request.POST.get('method',"")
+        url = request.POST.get('url',"")
+        params_key1 = repr(request.POST.get("key1","").encode('utf-8'))
+        logger.debug(params_key1)
+        params_value1 =repr(request.POST.get("value1","").encode('utf-8'))
+        logger.debug(params_value1)
+        ex_result = request.POST.get('ex_result',"")
         if title == ''or desc == ''or method == '' or url == '' or params_key1 == ''or params_value1 == '' or ex_result == '':
             #return JsonResponse({'status':10021,'message':'some paramters is null'})
             return render(request,'add.html',{'error': 'any empty input?'})
         else:
-            jsons_data['id'] = testcaseID
-            jsons_data['title'] = title
-            jsons_data['desc'] = desc
-            jsons_data['method'] = method
-            jsons_data['url'] = url
+            jsons_data["id"] = testcaseID
+            jsons_data["title"] = title
+            jsons_data["desc"] = desc
+            jsons_data["method"] = method
+            jsons_data["url"] = url
             params[params_key1] = params_value1
-            jsons_data['params'] = params
-            jsons_data['ex_result'] = ex_result
+            jsons_data["params"] = params
+            jsons_data["ex_result"] = ex_result
             store_json(jsons_data)
             return HttpResponseRedirect('/index/')
+
 #删除功能
 def delete_testcase(requset):
     id = requset.GET.get('id')
     delete_json(id)
     return HttpResponseRedirect('/index/')
+
+
+
 #编辑
 def edit_testcase(request):
     id = request.GET.get('id')
     obj = edit_json(id)
-    logger.debug(obj.params)
-    parms = {u'key':u'value'}
-    logger.debug(parms)
-    for k,v in parms.items():
+    s = obj.params
+    logger.debug(s)
+    logger.debug(type(s))
+    str = json.loads(s)
+    logger.debug(str)
+    logger.debug(type(str))
+    for k,v in str.items():
         logger.debug(k+':'+v)
-    return render(request,'edit.html',{'obj':obj,'params':parms})
+    return render(request,'edit.html',{'obj':obj,'params':str})
+
+
 
 def store_json(jsondata):
-    dic ={'case_id':jsondata['id'],'title':jsondata['title'],'desc':jsondata['desc'],'method':jsondata['method'],'url'
-    :jsondata['url'],'params':jsondata['params'],'ex_result':jsondata['ex_result']}
+    dic ={"case_id":jsondata["id"],"title":jsondata["title"],"desc":jsondata["desc"],"method":jsondata["method"],"url"
+    :jsondata["url"],"params":jsondata["params"],"ex_result":jsondata["ex_result"]}
     models.Testcase.objects.create(**dic)
+
 
 
 def load_json():
     jsondata = models.Testcase.objects.all()
     return jsondata
+
 #编辑或查看：id
 def edit_json(index):
     case = models.Testcase.objects.get(id=index)
@@ -90,6 +110,9 @@ def edit_json(index):
 #删除，参数：id
 def delete_json(index):
     models.Testcase.objects.filter(id=index).delete()
+
+
+
 #用例集合分页展示
 def test_cases(request):
     projectName = [u'慢钱',u'有单',u'保理']
